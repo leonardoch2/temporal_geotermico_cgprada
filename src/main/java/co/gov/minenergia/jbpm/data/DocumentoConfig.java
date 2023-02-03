807b07258150b8ccc8e56132e5d6aaddb21c5237
wbadmin <>
2023-02-03 08:20:25 -0500

    {/src/main/java/co/gov/minenergia/jbpm/data/DocumentoConfig.java}

--- src/main/java/co/gov/minenergia/jbpm/data/DocumentoConfig.java
+++ src/main/java/co/gov/minenergia/jbpm/data/DocumentoConfig.java
@@ -6,12 +6,141 @@ package co.gov.minenergia.jbpm.data;
 
 public class DocumentoConfig implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public DocumentoConfig() {
-    }
+	@org.kie.api.definition.type.Label(value = "Codigo Tipo Documental")
+	private java.lang.String tipoDocCodigo;
+	@org.kie.api.definition.type.Label(value = "Nombre Tipo Documental")
+	private java.lang.String tipoDocNombre;
+	@org.kie.api.definition.type.Label(value = "Incluir Radicados?")
+	private java.lang.Boolean incluirRadicados;
+	@org.kie.api.definition.type.Label(value = "Incluir Anexos?")
+	private java.lang.Boolean incluirAnexos;
+	@org.kie.api.definition.type.Label(value = "Anexo a Expediente?")
+	private java.lang.Boolean anexoExpediente;
+	@org.kie.api.definition.type.Label(value = "Es una comunicacion de entrada?")
+	private java.lang.Boolean esComunicacionEntrada;
+	@org.kie.api.definition.type.Label(value = "Es una comunicación de salida?")
+	private java.lang.Boolean esComunicacionSalida;
+	@org.kie.api.definition.type.Label(value = "Es una comunicación interna?")
+	private java.lang.Boolean esComunicacionInterna;
+	@org.kie.api.definition.type.Label(value = "Es una resolución?")
+	private java.lang.Boolean esResolucion;
+	@org.kie.api.definition.type.Label(value = "Es un auto?")
+	private java.lang.Boolean esAuto;
+	@org.kie.api.definition.type.Label(value = "Es un acta?")
+	private java.lang.Boolean esActa;
 
+	public DocumentoConfig() {
+	}
 
+	public java.lang.String getTipoDocCodigo() {
+		return this.tipoDocCodigo;
+	}
 
+	public void setTipoDocCodigo(java.lang.String tipoDocCodigo) {
+		this.tipoDocCodigo = tipoDocCodigo;
+	}
+
+	public java.lang.String getTipoDocNombre() {
+		return this.tipoDocNombre;
+	}
+
+	public void setTipoDocNombre(java.lang.String tipoDocNombre) {
+		this.tipoDocNombre = tipoDocNombre;
+	}
+
+	public java.lang.Boolean getIncluirRadicados() {
+		return this.incluirRadicados;
+	}
+
+	public void setIncluirRadicados(java.lang.Boolean incluirRadicados) {
+		this.incluirRadicados = incluirRadicados;
+	}
+
+	public java.lang.Boolean getIncluirAnexos() {
+		return this.incluirAnexos;
+	}
+
+	public void setIncluirAnexos(java.lang.Boolean incluirAnexos) {
+		this.incluirAnexos = incluirAnexos;
+	}
+
+	public java.lang.Boolean getAnexoExpediente() {
+		return this.anexoExpediente;
+	}
+
+	public void setAnexoExpediente(java.lang.Boolean anexoExpediente) {
+		this.anexoExpediente = anexoExpediente;
+	}
+
+	public java.lang.Boolean getEsComunicacionEntrada() {
+		return this.esComunicacionEntrada;
+	}
+
+	public void setEsComunicacionEntrada(java.lang.Boolean esComunicacionEntrada) {
+		this.esComunicacionEntrada = esComunicacionEntrada;
+	}
+
+	public java.lang.Boolean getEsComunicacionSalida() {
+		return this.esComunicacionSalida;
+	}
+
+	public void setEsComunicacionSalida(java.lang.Boolean esComunicacionSalida) {
+		this.esComunicacionSalida = esComunicacionSalida;
+	}
+
+	public java.lang.Boolean getEsComunicacionInterna() {
+		return this.esComunicacionInterna;
+	}
+
+	public void setEsComunicacionInterna(java.lang.Boolean esComunicacionInterna) {
+		this.esComunicacionInterna = esComunicacionInterna;
+	}
+
+	public java.lang.Boolean getEsResolucion() {
+		return this.esResolucion;
+	}
+
+	public void setEsResolucion(java.lang.Boolean esResolucion) {
+		this.esResolucion = esResolucion;
+	}
+
+	public java.lang.Boolean getEsAuto() {
+		return this.esAuto;
+	}
+
+	public void setEsAuto(java.lang.Boolean esAuto) {
+		this.esAuto = esAuto;
+	}
+
+	public java.lang.Boolean getEsActa() {
+		return this.esActa;
+	}
+
+	public void setEsActa(java.lang.Boolean esActa) {
+		this.esActa = esActa;
+	}
+
+	public DocumentoConfig(java.lang.String tipoDocCodigo,
+			java.lang.String tipoDocNombre, java.lang.Boolean incluirRadicados,
+			java.lang.Boolean incluirAnexos, java.lang.Boolean anexoExpediente,
+			java.lang.Boolean esComunicacionEntrada,
+			java.lang.Boolean esComunicacionSalida,
+			java.lang.Boolean esComunicacionInterna,
+			java.lang.Boolean esResolucion, java.lang.Boolean esAuto,
+			java.lang.Boolean esActa) {
+		this.tipoDocCodigo = tipoDocCodigo;
+		this.tipoDocNombre = tipoDocNombre;
+		this.incluirRadicados = incluirRadicados;
+		this.incluirAnexos = incluirAnexos;
+		this.anexoExpediente = anexoExpediente;
+		this.esComunicacionEntrada = esComunicacionEntrada;
+		this.esComunicacionSalida = esComunicacionSalida;
+		this.esComunicacionInterna = esComunicacionInterna;
+		this.esResolucion = esResolucion;
+		this.esAuto = esAuto;
+		this.esActa = esActa;
+	}
 
 }
